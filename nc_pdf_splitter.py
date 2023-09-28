1689a166e2e9bfc38a2515ee0ac1196260d30f28
twizzlard <76930650+twizzlard@users.noreply.github.com>
2023-09-27 23:05:29 -0400

    Update nc_pdf_splitter.py

--- nc_pdf_splitter.py
+++ nc_pdf_splitter.py
@@ -26,22 +26,23 @@ if uploaded_file and uploaded_file.name == 'OYSTER_BAY_RS5.pdf':
         st.write("\nTables extracted from the PDF:")
         for idx, table in enumerate(pdf_tables, start=1):
             df = pd.DataFrame(table)  # Convert the table to a DataFrame
-            combined_df = pd.concat([combined_df, df], ignore_index=True)
-            # st.write(f"Table {idx}:")
-            # st.write(df)
+            combined_df = pd.concat([combined_df, df], ignore_index=True)  # Combine the DataFrames
+            st.write(f"Table {idx}:")
+            st.write(df)
 
-        # st.write("\nCombined DataFrame:")
-        # st.write(combined_df)
+        st.write("\nCombined DataFrame:")
+        st.write(combined_df)
 
         # Create a temporary Excel file and save the combined DataFrame to it
         excel_file = NamedTemporaryFile(delete=False, suffix=".xlsx")
         combined_df.to_excel(excel_file.name, index=False)
 
-        # Download the Excel file
-        st.download_button("Download Excel", data=open(excel_file.name, "rb").read(), file_name="combined_data.xlsx", key="excel-download")
-        
+        # Provide a download link for the Excel file
+        st.markdown(
+            f"**[Download Excel File](data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{excel_file.name})**",
+            unsafe_allow_html=True
+        )
     else:
         st.write("\nNo tables found in the PDF.")
 else:
     st.write("Please upload the file named 'OYSTER_BAY_RS5.pdf'.")
-
